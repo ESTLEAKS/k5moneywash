a506d97281a70e2f24f0a878634d9c6cc76440e5
ESTLEAKS <leaksmidnightmafia@gmail.com>
2025-10-21 23:55:58 -0400

    Add English localization for money washing feature

---
+++ en.lua
@@ -0,0 +1,30 @@
+Locales['en'] = {
+    -- General
+    ['press_to_wash'] = 'Press ~INPUT_CONTEXT~ to wash money',
+    ['washing_money'] = 'Washing money...',
+    ['cancelled'] = 'Washing cancelled',
+    
+    -- Success Messages
+    ['wash_success'] = 'You washed ~g~$%s~s~ and received ~g~$%s~s~ clean money',
+    ['wash_success_no_tax'] = 'You washed ~g~$%s~s~ successfully',
+    
+    -- Error Messages
+    ['no_dirty_money'] = 'You don\'t have any dirty money',
+    ['not_enough_dirty'] = 'You need at least ~r~$%s~s~ to wash',
+    ['amount_too_high'] = 'You can only wash up to ~r~$%s~s~ at a time',
+    ['no_laundry_card'] = 'You need a ~r~Laundry Card~s~ to wash money',
+    ['no_access'] = 'You don\'t have access to this location',
+    ['wrong_job'] = 'You need to be a ~r~%s~s~ (Grade %s+) to use this',
+    ['invalid_amount'] = 'Invalid amount entered',
+    
+    -- Input Prompts
+    ['enter_amount'] = 'Enter amount to wash (Min: $%s | Max: $%s)',
+    ['amount_prompt'] = 'Amount:',
+    
+    -- Info Messages
+    ['tax_applied'] = 'Tax: ~r~%s%%~s~ (~r~$%s~s~ deducted)',
+    ['card_consumed'] = 'Laundry Card used',
+    
+    -- Blip
+    ['blip_name'] = 'Money Wash',
+}
